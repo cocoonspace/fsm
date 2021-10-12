041863a699e3792f5b6c56b26a359c0ab1dfdf07
jfbus <jf@bustarret.com>
2021-10-12 22:03:25 +0200

    comments, doc, README

--- doc.go
+++ doc.go
@@ -16,7 +16,7 @@ Package fsm allows you to add Finite State Machines to your code.
 		fsm.Dst(StateBar),
 	)
 
-You can have custom checks or actions :
+You can have custom checks or actions:
 
 	f.Transition(
 		fsm.Src(StateFoo), fsm.Check(func() bool {
@@ -26,6 +26,29 @@ You can have custom checks or actions :
 			// do something
 		}),
 	)
+
+
+Transitions can be triggered the second time an event occurs:
+
+	f.Transition(
+		fsm.On(EventFoo), fsm.Src(StateFoo), fsm.Times(2),
+		fsm.Dst(StateBar),
+	)
+
+Functions call be called when entering or leaving a state:
+
+	f.EnterState(StateFoo, func() {
+		// do something
+	})
+	f.Enter(func(state fsm.State) {
+		// do something
+	})
+	f.ExitState(StateFoo, func() {
+		// do something
+	})
+	f.Exit(func(state fsm.State) {
+		// do something
+	})
 
 */
 package fsm
